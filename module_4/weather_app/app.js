fec9404b60325ae92f040bf313fd9d8efbf4125a
Jack <jack.x.chen@outlook.com>
2023-10-22 21:38:55 +1000

    Added skeleton for weather app

---
+++ module_4/weather_app/app.js
@@ -0,0 +1,31 @@
+import * as http from "http";
+require("dotenv").config();
+
+const API_KEY = process.env.WEATHERAPI_KEY;
+const WEATHERAPI_BASE = "http://api.weatherapi.com/v1";
+
+async function weather(res) {
+    fetch(`${WEATHERAPI_BASE}/current.json?key=${API_KEY}&q=Brisbane`)
+    .then(res => res.json())
+    .then(data => {
+        res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
+        res.write(JSON.stringify(data));
+        res.end();
+    });
+    
+}
+
+function routing(req, res) {
+  const url = req.url;
+  const method = req.method;
+  if (url.startsWith("/weather") && method == "GET") {
+    weather(res);
+  } else {
+    // No page matched the url
+    res.write("No matching page");
+    res.end();
+  }
+}
+createServer(routing).listen(3000, function () {
+  console.log("server start at port 3000"); //the server object listens on port
+});

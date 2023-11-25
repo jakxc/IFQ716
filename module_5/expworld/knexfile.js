435928b16fc3d5c6c52be29c23a18ba206a536b2
Jack <jack.x.chen@outlook.com>
2023-11-25 21:50:50 +1000

    Added cors and post endpoint

--- module_5/expworld/knexfile.js
+++ module_5/expworld/knexfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
         client: 'mysql2',
         connection: {
-        host: '127.0.0.1',
-        database: 'world',
-        user: 'root',
-        password: 'Y8acl9in9985zkl61@'
-    }
+            host: process.env.DB_HOST,
+            database: 'world',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS
+        }
 }
--- module_5/expworld/routes/index.js
+++ module_5/expworld/routes/index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -9,31 +8,72 @@ router.get("/", function (req, res, next) {
 
 
 router.get("/api/city", function (req, res, next) {
-  req.db
+  const name = req.query.name
+  const countryCode = req.query.countryCode; //either a value or undefined
+  const limit = req.query.limit;
+
+  if (name || countryCode) {
+    req.db
+    .from("city")
+    .select("*")
+    .where("CountryCode", countryCode)
+    .then((rows) => {
+      const limitedRows = rows.slice(0, limit);
+      res.json({ Error: false, Message: "Success", City: limitedRows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+    });
+  } else {
+    req.db
     .from("city")
     .select("name", "district")
     .then((rows) => {
-      res.json({ Error: false, Message: "Success", City: rows });
+      const limitedRows = rows.slice(0, limit);
+      res.json({ Error: false, Message: "Success", City: limitedRows });
     })
     .catch((err) => {
       console.log(err);
       res.json({ Error: true, Message: "Error in MySQL query" });
     });
+  }
 });
 
 
-router.get("/api/city/:CountryCode", function (req, res, next) {
-  req.db
-    .from("city")
-    .select("*")
-    .where("CountryCode", "=", req.params.CountryCode)
-    .then((rows) => {
-      res.json({ Error: false, Message: "Success", City: rows });
+// router.get("/api/city/:CountryCode", function (req, res, next) {
+//   req.db
+//     .from("city")
+//     .select("*")
+//     .where("CountryCode", "=", req.params.CountryCode)
+//     .then((rows) => {
+//       res.json({ Error: false, Message: "Success", City: rows });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.json({ Error: true, Message: "Error in MySQL query" });
+//     });
+// });
+
+router.post('/api/update', (req, res) => {
+  const city = req.body.City;
+  const countryCode = req.body.CountryCode;
+  const pop = req.body.Pop
+  if (!city || !countryCode || !pop) {
+      res.status(400).json({ message: `Error updating population` });
+      console.log(`Error on request body:`, JSON.stringify(req.body));
+  } else { 
+    req.db('city').where({"Name": city, "CountryCode": countryCode}).update({
+      "Population": pop
     })
-    .catch((err) => {
-      console.log(err);
-      res.json({ Error: true, Message: "Error in MySQL query" });
-    });
+      .then(_ => {
+      res.status(201).json({ message: `Successful update ${city}`});
+      console.log(`successful population update:`, JSON.stringify({"Population": pop}));
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Database error - not updated' });
+    })
+  } 
 });
 
 module.exports = router;
